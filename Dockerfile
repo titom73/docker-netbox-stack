12f5cf05fda8f020e1b36d26238df32d30d10f74
Thomas Grimonet <titom73@users.noreply.github.com>
2023-04-05 09:37:29 +0000

    feat: Add ipscaner script

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,13 @@ ARG NETBOX_VERSION=latest
 FROM netboxcommunity/netbox:${NETBOX_VERSION}
 
 RUN apt-get update && \
+<<<<<<< HEAD
         apt-get install --yes -qq --no-install-recommends \
                 inetutils-ping
+=======
+	apt-get install --yes -qq --no-install-recommends \
+		inetutils-ping
+>>>>>>> 5a2682f (feat: set default timezone)
 
 COPY ./configuration/plugin_requirements.txt /
 RUN /opt/netbox/venv/bin/pip install  --no-warn-script-location -r /plugin_requirements.txt
---
+++ scripts/netbox_ipscanner.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+# coding: utf-8 -*-
+#
+# Copyright 2023 Arista Networks Thomas Grimonet
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http: //www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from typing import Any
+import pynetbox, urllib3, networkscan, socket, ipaddress
+from extras.scripts import Script
+
+"""
+Netbox Script to scan prefixes and update IP address status
+
+Description
+-----------
+
+It gets all prefixes configured in netbox and if tag <TAG_FOR_SCAN> is configured, then scan all ip_addresses
+- If host is UP: mark IP as active
+- If host is Down and already configured, mark IP as deprecated
+- Try to resolve DNS to report in netbox
+- Get VRF from that prefix and configure IP address in this VRF.
+
+Requirements
+------------
+
+- ipcalc
+- networkscan
+- pynetbox
+
+inetutils-ping MUST be installed in your netbox environment
+
+Unsupported features
+--------------------
+
+- IP addresses in different VRFs.
+"""
+
+# Netbox URL to use
+NETBOX_URL = 'http://netbox.as73.inetsix.net'
+# Netbox Token to authenticate against NETBOX_URL
+NETBOX_TOKEN = 'd02bc3922f88f28e48fc66e9a3b3c8e3eab7a79a'
+# Tag configured on prefixes to scan
+TAG_FOR_SCAN = 'ipscan'
+
+
+class IpScan(Script):
+    class Meta:
+        name = 'Netbox IP Scanner'
+        description = 'Scans prefixes configured in Netbox and updates IP Address under IPAM module'
+
+    def run(self, data: Any, commit: bool) -> None:
+        def do_rdns(ip) -> str:
+            """
+            Execute reverse-dns query to discover hostname for IP address
+
+            Args:
+                ip (str): IP address to resolv
+
+            Returns:
+                str: Hostname configured for IP address (PTR)
+            """
+            try:
+                data = socket.gethostbyaddr(ip)
+            except Exception:
+                return '' # fails gracefully
+            return '' if data[0] == '' else data[0]
+
+        nb_instance = pynetbox.api(url=NETBOX_URL, token=NETBOX_TOKEN)
+        nb_instance.http_session.verify = False
+        self.log_warning('connecting to netbox')
+
+        # get all available subnets
+        prefixes = nb_instance.ipam.prefixes.all()
+        for prefix in prefixes:
+            is_scannable = False
+            for tag in prefix.tags:
+                is_scannable = True if str(tag).lower() == str(TAG_FOR_SCAN).lower() else is_scannable
+            if is_scannable:
+                self.log_info(f'scanning prefix {prefix} since tag {TAG_FOR_SCAN} is configured')
+
+                # get VRF configured for the prefix
+                configured_vrf = str(prefix.vrf) if prefix.vrf is not None else ''
+
+                ipv4network = ipaddress.IPv4Network(prefix)
+                scan = networkscan.Networkscan(prefix)
+
+                # run network scan. require inetutils-ping to be installed on runner
+                scan.run()
+                self.log_info(f'scan of subnet {prefix} done.')
+                if scan.list_of_hosts_found == []:
+                    self.log_warning(f'No host found in network {prefix}')
+                else:
+                    self.log_success(f'Found: {len(scan.list_of_hosts_found)} IPs in {prefix}')
+                    self.log_success(f'-> {scan.list_of_hosts_found}')
+
+                # Build a list of IP to update
+                # Concatenate live host and Netbox configured hosts
+                netbox_addresses = [str(address).split('/')[0] for address in nb_instance.ipam.ip_addresses.filter(parent=prefix)]
+                ips_to_check = scan.list_of_hosts_found + netbox_addresses
+
+                # Monitor IP address status from Network to Netbox
+                for address_scanned in ips_to_check:
+                    # Build CIDR format as it is used by Netbox
+                    address_scanned_cidr = f'{address_scanned}/{ipv4network.prefixlen}'
+                    # Get Netbox information for given IP Address
+                    address_netbox_info = nb_instance.ipam.ip_addresses.get(address=address_scanned_cidr)
+                    # Execute reverse DNS check for IP address
+                    rdns = do_rdns(address_scanned)
+
+                    # If IP already exist in Netbox
+                    if address_netbox_info != None:
+                        # self.log_debug(f'got {address_netbox_info} to deal with')
+                        state = 'reserved'
+                        # If present in scan list, mark it as active.
+                        if str(address_netbox_info).split('/')[0] in scan.list_of_hosts_found:
+                            state = 'active'
+                        # If present in Netbox but not in scan result
+                        # Mark it as deprecated
+                        else:
+                            self.log_warning(f'Host {address_netbox_info} is not responding. updating state to deprecated')
+                            state = 'deprecated'
+
+                        # Update IP address in Netbox
+                        nb_instance.ipam.ip_addresses.update([{'id':address_netbox_info.id, 'status':state, 'vrf': nb_instance.ipam.vrfs.get(q=configured_vrf).id, 'dns_name':rdns},])
+
+                    # For new IP detected in network
+                    else:
+                        result = nb_instance.ipam.ip_addresses.create(address=address_scanned_cidr, status='active', dns_name=rdns)
+                        result.vrf = nb_instance.ipam.vrfs.get(q=configured_vrf).id
+                        result.save()
